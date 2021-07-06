cf0b018718c93e913d4c2fc05f36231c50d825c8
= <marudhupaandian@gmail.com>
2021-07-06 11:27:09 +0530

    split the program into smaller parts and added different modes

--- profiler.py
+++ profiler.py
@@ -1,5 +1,7 @@
 # Set of valid characters for a password
-char_set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_ "
+alpha = "abcdefghijklmnopqrstuvwxyz"
+numeric = "1234567890"
+symbols = "!@#$%^&*()_ "
 
 
 # Main function
@@ -8,21 +10,49 @@ def main():
     # Giving the starting banner
     banner()
 
+    # Asking the user to choose what to do
+    option = input("what do you want to do? (i for interactive mode, l for password list generation, h for help, e for exit)\n>")
+    if option == 'i':
+        interactive()
+    elif option == 'l':
+        creat_word_list()
+    elif option == 'h':
+        print_help()
+    elif option == 'e':
+        print("bye")
+        exit()
+    else:
+        print("invalid option!")
+
+
+# The function to run in interactive mode and make a customized password list
+def interactive():
+
     # The basic info dict
     target_info = {}
 
     # Getting basic info
-    target_info["name"] = input("Name of the target:\n>>> ")
-    target_info["nick_name"] = input("Nick name of the target:\n>>> ")
-    target_info["DOB"] = input("Date Of Birth(dd-mm-yyyy):\n>>> ")
-    target_info["lucky_num"] = input("Lucky Number of the target:\n>>> ")
+    target_info["name"] = input("Name of the target:\n> ")
+    target_info["nick_name"] = input("Nick name of the target:\n> ")
+    target_info["DOB"] = input("Date Of Birth(dd-mm-yyyy):\n> ")
+    target_info["lucky_num"] = input("Lucky Number of the target:\n> ")
 
     # Printing the collected info
     for key, value in target_info.items():
         print(f"{key}: {value}")
 
 
-# Start up banner
+# The fuction to create the password list based on give coditions
+def creat_word_list():
+    pass
+
+
+# The help page
+def print_help():
+    pass
+
+
+# The Start up banner
 def banner():
     print(r"""
 ==================================================================================================================
