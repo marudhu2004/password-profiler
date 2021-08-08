ebb6277d38596f7e4249d73e0564dc2b0409b26f
= <marudhupaandian@gmail.com>
2021-08-08 22:05:08 +0530

    added password list generator function updated gitignore to include pycache

--- profiler.py
+++ profiler.py
@@ -15,14 +15,14 @@ def main():
     if option == 'i':
         interactive()
     elif option == 'l':
-        creat_word_list()
+        create_word_list()
     elif option == 'h':
         print_help()
     elif option == 'e':
         print("bye")
         exit()
     else:
-        print("invalid option!")
+        print("invalid option!, exiting")
 
 
 # The function to run in interactive mode and make a customized password list
@@ -43,8 +43,53 @@ def interactive():
 
 
 # The fuction to create the password list based on give coditions
-def creat_word_list():
-    pass
+def create_word_list():
+    
+    # Getting the basic settings 
+    settings = {'num': False, 'special': False, "length": (3, 3)}
+
+    # Getting the character set setup
+    char_set = alpha + alpha.upper()
+    if settings['num']:
+        char_set += numeric
+    if settings['special']:
+        char_set += symbols
+    
+    # Setting up length variables
+    max_len = settings['length'][1]
+    min_len = settings['length'][0]
+
+    # Creating the inital string to iterate upon
+    password_string = char_set[0] * min_len
+    char_pass_index = min_len - 1
+
+    while True:
+        password_string = manage_carry(len(password_string) - 1, char_set, password_string)
+        if len(password_string) <= max_len: break
+        print(password_string)
+
+
+# Rcursive function for next char update
+def manage_carry(char_pass_index, char_set, current_pass):
+    
+    # In case a new character is needed to be added
+    if char_pass_index < 0:
+        return char_set[0] + current_pass
+
+    # Getting current char index in char_set
+    char_index = char_set.index(current_pass[char_pass_index])
+    pass_len = len(current_pass)
+    char_set_len = len(char_set)
+    
+    # Checking if the current char needs to be carried again
+    if char_index >= char_set_len - 1:
+        new_char = char_set[(char_index + 1) % char_set_len]
+        new_pass = current_pass[0 : char_pass_index] + new_char + current_pass[char_pass_index + 1 : pass_len]
+        return manage_carry(char_pass_index - 1, char_set, new_pass)
+    
+    # Returning the new password string
+    new_char = char_set[(char_index + 1) % char_set_len]
+    return current_pass[0 : char_pass_index] + new_char + current_pass[char_pass_index + 1 : pass_len]
 
 
 # The help page
@@ -70,4 +115,6 @@ def banner():
 
 # Execution part
 if __name__ == "__main__":
-    main()
+
+    create_word_list()
+    # main()
