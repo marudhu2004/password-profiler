c3d6e56121c6b7ae62fd01d805f8acd0043f9e75
= <marudhupaandian@gmail.com>
2021-10-31 23:08:32 +0530

    updated the generator to an object orientd approach and updated the testcases

--- password_profiler/generator.py
+++ password_profiler/generator.py
@@ -4,139 +4,141 @@ numeric = "1234567890"
 symbols = "!@#$%^&*()_ "
 
 
-# The fuction to create the password list based on give coditions
-def create_word_list(out_file, num = False, special = False, min_len = 3, max_len = 3, upper = False, lower = True, char_set=""):
+class PasswordGenerator:
 
-    # Creating the generator object
-    password_list = password_list_generator(num, special, min_len, max_len, upper, lower, char_set)
-
-    # Opening the output file    
-    with open(out_file, 'w') as file:
-
-        # Writing the passwords to the output file
-        try:
-            for password in password_list:
-                file.write(password + '\n')
+    # Setting up the necessary settings for the generator while creating the object
+    def __init__(self, num = False, special = False, min_len = 3, max_len = 3, upper = False, lower = True, char_set=""):
         
-        except ValueError:
-            print('the charset must not be empty plz try with different settings')
-            return
-
-# Rcursive function for next char update
-def next_pass(char_pass_index, char_set, current_pass):
-    
-    # In case a new character is needed to be added
-    if char_pass_index < 0:
-        return char_set[0] + current_pass
-
-    # Getting current char index in char_set
-    char_index = char_set.index(current_pass[char_pass_index])
-    pass_len = len(current_pass)
-    char_set_len = len(char_set)
-    
-    # Creating new password
-    new_char = char_set[(char_index + 1) % char_set_len]
-    new_pass = current_pass[0 : char_pass_index] + new_char + current_pass[char_pass_index + 1 : pass_len]
-    
-    # Checking if the current char needs to be carried again
-    if char_index >= char_set_len - 1:
-        return next_pass(char_pass_index - 1, char_set, new_pass)
-    
-    # Returning the new password string
-    return new_pass
-
-# Generator version of getting passwords
-def password_list_generator(num = False, special = False, min_len = 3, max_len = 3, upper = False, lower = True, char_set=""):
-    
-    # Getting the character set setup
-    charset = ""
-    if lower:
-        charset += alpha
-    if upper:
-        charset += alpha.upper()
-    if num:
-        charset += numeric
-    if special:
-        charset += symbols
-
-    # Checking if charset is not empty
-    if charset == "":
-        raise ValueError("there must be atleast 1 character in char set! (check the function settings)")
-
-    # If a specific charset is specified
-    if char_set:
-        charset = char_set
-    
-    # Creating the inital string to iterate upon
-    password_string = charset[0] * min_len
-    char_pass_index = min_len - 1
-
-    # Looping to get the possible combinations
-    while True:
-            
-        # Leaving if the last pass reached
-        if len(password_string) > max_len: break
-
-        # Writing the password to the file
-        yield password_string
+        # Setting the maximum and minimum lengths for the passowrds
+        self.min_len = min_len
+        self.max_len = max_len
         
-        # Getting the new password strings
-        password_string = next_pass(len(password_string) - 1, charset, password_string)
+        # Getting the character set setup
+        charset = ""
+        if lower:
+            charset += alpha
+        if upper:
+            charset += alpha.upper()
+        if num:
+            charset += numeric
+        if special:
+            charset += symbols
+        
+        self.charset = charset
+
+        # Using the specific charset if provided
+        if char_set:
+            self.charset = char_set
+
+        # Raising an error if the charset is empty
+        if charset == '':
+            raise ValueError('empty charset [check settings to make sure there is atleast 1 character]')
 
 
+    # The fuction to create the password list based on give coditions
+    def create_word_list(self, out_file):
 
-# To deal with knowing specific part chars
-def with_known_chars(known_chars, symbol, outfile, num = False, special = False, upper = False, lower = True, char_set=""):
+        # Creating the generator object
+        password_list = self.password_list_generator()
 
-    # Getting the mask and length needed to fill
-    lenght = len(known_chars)
-    mask = [1 if i == symbol else 0 for i in known_chars]
-    gen_len = sum(mask)
+        # Opening the output file    
+        with open(out_file, 'w') as file:
 
-    # Creating the charset to check if the call is valid
-    charset=""
-    if lower:
-        charset += alpha
-    if upper:
-        charset += alpha.upper()
-    if num:
-        charset += numeric
-    if special:
-        charset += symbols
-
-    # raising an error if there is nothing to substitute with
-    if gen_len == 0:
-        raise ValueError("there should atleast be one character to be substitute in the password") 
-    
-    # Raising an error if the symbol is in the charset
-    if symbol in charset:
-        raise ValueError("The symbol must not be a part of the charset")
-
-    # Creating paswords with the given conditions
-    fill_vals = password_list_generator(num , special, gen_len, gen_len, upper, lower, char_set)
-    
-    # Opening the output file to write to
-    with open(outfile, 'w') as file:
-
-        # Going through the list of generated passwords
-        for value in fill_vals:
-            password = ""
-            count = 0
+            # Writing the passwords to the output file
+            try:
+                for password in password_list:
+                    file.write(password + '\n')
             
-            # Assigining the generated characters requires positions
-            for i, v in enumerate(mask):
-                # If generated value needed
-                if v:
-                    password += value[count]
-                    count += 1
+            except ValueError:
+                print('the charset must not be empty plz try with different settings')
+                return
 
-                # If value alreaady known
-                else:
-                    password += known_chars[i]
 
-            # Saving the generated password
-            file.write(password+"\n")
+    # Rcursive function for next char update
+    def next_pass(self, char_pass_index, current_pass):
+        
+        # In case a new character is needed to be added
+        if char_pass_index < 0:
+            return self.charset[0] + current_pass
+
+        # Getting current char index in char_set
+        char_index = self.charset.index(current_pass[char_pass_index])
+        pass_len = len(current_pass)
+        char_set_len = len(self.charset)
+        
+        # Creating new password
+        new_char = self.charset[(char_index + 1) % char_set_len]
+        new_pass = current_pass[0 : char_pass_index] + new_char + current_pass[char_pass_index + 1 : pass_len]
+        
+        # Checking if the current char needs to be carried again
+        if char_index >= char_set_len - 1:
+            return self.next_pass(char_pass_index - 1, new_pass)
+        
+        # Returning the new password string
+        return new_pass
+
+
+    # Generator version of getting passwords
+    def password_list_generator(self):
+        
+        # Creating the inital string to iterate upon
+        password_string = self.charset[0] * self.min_len
+        char_pass_index = self.min_len - 1
+
+        # Looping to get the possible combinations
+        while True:
+                
+            # Leaving if the last pass reached
+            if len(password_string) > self.max_len: break
+
+            # Writing the password to the file
+            yield password_string
+            
+            # Getting the new password strings
+            password_string = self.next_pass(len(password_string) - 1, password_string)
+
+
+    # To deal with knowing specific part chars
+    def with_known_chars(self, known_chars, symbol, outfile):
+
+        # Getting the mask and length needed to fill
+        lenght = len(known_chars)
+        mask = [1 if i == symbol else 0 for i in known_chars]
+        gen_len = sum(mask)
+
+        # raising an error if there is nothing to substitute with
+        if gen_len == 0:
+            raise ValueError("there should atleast be one character to be substitute in the password") 
+        
+        # Raising an error if the symbol is in the charset
+        if symbol in self.charset:
+            raise ValueError("The symbol must not be a part of the charset")
+
+        # Creating paswords with the given conditions
+        fill_vals = self.password_list_generator()
+        
+        # Opening the output file to write to
+        with open(outfile, 'w') as file:
+
+            # Going through the list of generated passwords
+            for value in fill_vals:
+                password = ""
+                count = 0
+                
+                # Assigining the generated characters requires positions
+                for i, v in enumerate(mask):
+                    # If generated value needed
+                    if v:
+                        password += value[count]
+                        count += 1
+
+                    # If value alreaady known
+                    else:
+                        password += known_chars[i]
+
+                # Saving the generated password
+                file.write(password+"\n")
 
 
 if __name__ == '__main__':
-    with_known_chars("max", '~', 'test.txt')
+    with_known_chars("~K~m~a~~x~", '~', 'test.txt')
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -4,21 +4,22 @@ from password_profiler.generator import *
 from util import check_file
 
 class TestNextPass(unittest.TestCase):
+
+    def setUp(self):
+        self.generator = PasswordGenerator()        
     
     def test_increment_one_char(self):
 
         # Checking for one char increment
         current_pass = "hello"
-        char_set = alpha
-        result = next_pass(len(current_pass) - 1, char_set, current_pass)
+        result = self.generator.next_pass(len(current_pass) - 1, current_pass)
         self.assertEquals(result, 'hellp')
 
     def test_one_carry(self):
         
         # Checking for one char carry
         current_pass = "hellz"
-        char_set = alpha
-        result = next_pass(len(current_pass) - 1, char_set, current_pass)
+        result = self.generator.next_pass(len(current_pass) - 1, current_pass)
         self.assertEquals(result, 'helma')
 
     def test_multiple_carry(self):
@@ -26,53 +27,47 @@ class TestNextPass(unittest.TestCase):
         # Dealing with multiple carries
         current_pass = "hezzz"
         char_set = alpha
-        result = next_pass(len(current_pass) - 1, char_set, current_pass)
+        result = self.generator.next_pass(len(current_pass) - 1, current_pass)
         self.assertEquals(result, 'hfaaa')
 
     def test_add_new_char(self):
 
         # Dealing with addition of new character
         current_pass = "zzzz"
-        char_set = alpha
-        result = next_pass(len(current_pass) - 1, char_set, current_pass)
+        result = self.generator.next_pass(len(current_pass) - 1, current_pass)
         self.assertEquals(result, 'aaaaa')
 
 
 class TestCreateWordList(unittest.TestCase):
 
+    def setUp(self):
+        self.generator = PasswordGenerator()
+
     def test_fixed_len_passwords(self):
 
         # Creating the wordlist
-        create_word_list('tests/out.txt', min_len=3, max_len=3)
+        self.generator.create_word_list('tests/out.txt')
         
         # Checking if the wordlist is properly generated
         check_file(self, 'tests/out.txt', 'tests/wordlists/same_length.txt')
-
     
     def test_varying_len_passwords(self):
 
         # Creating the wordlist
-        create_word_list('tests/out.txt', min_len=3, max_len=4)
+        self.generator.max_len = 4
+        self.generator.create_word_list('tests/out.txt')
         
         # Checking if the wordlist is properly generated
         check_file(self, 'tests/out.txt', 'tests/wordlists/varying_lengths.txt')
 
-
     def test_specific_charset(self):
 
         # Creating the wordlist
-        create_word_list('tests/out.txt', min_len=3, max_len=3, char_set='1234')
+        generator = PasswordGenerator(char_set='1234')
+        generator.create_word_list('tests/out.txt')
 
         # Checking if the wordlist is properly generated
         check_file(self, 'tests/out.txt', 'tests/wordlists/specific_charset.txt')
-
-
-    def test_empty_charset(self):
-        create_word_list('out.txt', lower=False)
-
-        # Checking if a output file was created
-        self.assertFalse(os.path.exists('tests/out.txt'))
-
 
     def tearDown(self):
         path = os.path.join(os.getcwd(), 'tests/out.txt')
@@ -84,18 +79,21 @@ class TestCreateWordList(unittest.TestCase):
 
 class TestWithKnownChars(unittest.TestCase):
 
+    def setUp(self):
+        self.generator = PasswordGenerator()
+
     def test_base_case(self):
 
         # Generating the password list
-        with_known_chars("m~ax~~", '~', 'out.txt')
+        self.generator.with_known_chars("m~ax~~", '~', 'out.txt')
 
         # Checking if its as expected
         check_file(self, 'out.txt', 'tests/wordlists/base_substitution_case.txt')
 
     def test_no_char_to_substitute(self):
         with self.assertRaises(ValueError):
-            with_known_chars("hello", '~', 'out.txt')
+            self.generator.with_known_chars("hello", '~', 'out.txt')
     
     def test_symbol_in_charset(self):
         with self.assertRaises(ValueError):
-            with_known_chars("helloa", 'a', 'out.txt')
+            self.generator.with_known_chars("helloa", 'a', 'out.txt')
